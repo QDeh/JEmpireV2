1dc01081c15897a0824467b4d4c756fdfad249d1
PEREIRA Arthur <arthur.pereira@externe.maif.fr>
2026-02-24 15:35:59 +0100

    Création de la class Soldier

---
+++ src/units/Soldier.java
@@ -0,0 +1,11 @@
+package units;
+
+public class Soldier extends Units {
+   
+    public Soldier(int hp, int damage) {
+        super(hp, damage);
+        this.hp = 20;
+        this.damage = 2;
+    }
+    
+}
