db5d51dda4dea3ba5188ff4fcf5ad336287b0730
PEREIRA Arthur <arthur.pereira@externe.maif.fr>
2026-02-24 14:56:27 +0100

    Création de la class Units

---
+++ src/units/Units.java
@@ -0,0 +1,28 @@
+package units;
+
+public abstract class Units {
+    int hp = 10;
+    int damage = 1;
+    
+    public Units(int hp, int damage) {
+        this.hp = hp;
+        this.damage = damage;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+   
+}
