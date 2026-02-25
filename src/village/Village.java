d9c1b7c19749ea26dfd0d9bb8f6d0958a8cc07b4
PEREIRA Arthur <arthur.pereira@externe.maif.fr>
2026-02-25 16:21:12 +0100

    Création d'items

--- src/village/Village.java
+++ src/village/Village.java
@@ -1,6 +1,7 @@
 package village;
 
 import building.*;
+import items.*;
 import units.*;
 import ressources.*;
 import java.util.List;
@@ -10,12 +11,13 @@ public class Village {
     private String name;
     private List<Building> buildings = new ArrayList<>();
     private List<Units> units = new ArrayList<>();
+    private List<Items> items = new ArrayList<>();
 
-    
-    public Village(String name, List<Building> buildings, List<Units> units) {
+    public Village(String name, List<Building> buildings, List<Units> units, List<Items> items) {
         this.name = name;
         this.buildings = buildings;
         this.units = units;
+        this.items = items;
     }
 
     public String getName() {
@@ -30,4 +32,31 @@ public class Village {
         return units;
     }
 
+    public List<Items> getItems() {
+        return items;
+    }
+
+    public Weapon createWeapon(String name, int modifier, int level) {
+        Weapon newWeapon = new Weapon(name, modifier, level);
+        System.out.println("Arme forgée : " + newWeapon.getName() + " (Dégâts: " + newWeapon.getModifier() + ")");
+        return newWeapon;
+    }
+
+    public Tools createTool(String name, int modifier, int level) {
+        Tools newTool = new Tools(name, modifier, level);
+        System.out.println("Outil fabriqué : " + newTool.getName() + " (Modificateur: " + newTool.getModifier() + ")");
+        return newTool;
+    }
+
+    public void listItems() {
+        System.out.println("Liste des armes et outils disponibles dans le village :");
+        for (Units unit : units) {
+            if (unit.getItem() != null) {
+                System.out.println("- " + unit.getItem().getName() + " (Modificateur: " + unit.getItem().getModifier() + ")");
+            }
+        }
+    }
+    public Weapon epee = createWeapon("Sword", 5, 1);
+    public Tools pioche = createTool("Pickaxe", 3, 1);
+    
 }
