29b7a01be641787b13e3b450714000e39b4af8c9
QDeh <111422585+QDeh@users.noreply.github.com>
2026-02-25 09:23:51 +0100

    Update src/ressources/Ressources.java

--- src/ressources/Ressources.java
+++ src/ressources/Ressources.java
@@ -1,7 +1,12 @@
 package ressources;
 
 public abstract class Ressources {
-    private int quantity;
+    private int wood = 0;
+    private int stone = 0;
+    private int iron = 0;
+    private int gold = 0;
+    private int food = 100;
+    
 
     public Ressources(int quantity) {
         this.quantity = quantity;
