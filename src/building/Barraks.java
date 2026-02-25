76d8d97a8999a3c99753e6b8460895ba4c83add5
QDeh <111422585+QDeh@users.noreply.github.com>
2026-02-25 09:32:08 +0100

    feat: ajout des classes des bâtiments (#17)
    Co-authored-by: DEHAINAULT Quentin <quentin.dehainault@maif.fr>

---
+++ src/building/Barraks.java
@@ -0,0 +1,10 @@
+package building;
+
+public class Barraks extends Building {
+
+    public Barraks(int level) {
+        super(level);
+    }
+
+    // faire une fonction pour récupérer les ressources en fonction du niveau du batiment et du nombre d'unités présentes; +1 pour la créer
+}
---
+++ src/building/House.java
@@ -0,0 +1,10 @@
+package building;
+
+public class House extends Building {
+
+    public House(int level) {
+        super(level);
+    }
+
+    // faire une fonction pour récupérer la capacité en fonction du niveau du batiment et une pour la créer
+}
