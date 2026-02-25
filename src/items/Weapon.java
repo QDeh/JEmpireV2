59554889e48024c6b7c5f2dd378f91b1f24c05a6
PEREIRA Arthur <arthur.pereira@externe.maif.fr>
2026-02-25 14:21:29 +0100

    feat: equipItems

---
+++ src/items/Weapon.java
@@ -0,0 +1,9 @@
+package items;
+
+public class Weapon extends Items {
+
+    public Weapon(String name, int modifier, int level) {
+        super("Sword", 5, level);
+    }
+
+}
--- src/units/Chief.java
+++ src/units/Chief.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Chief extends Soldier {
     
     public Chief(int hp, int damage) {
-        super(30, 5);
+        super(30, 5,null);
        
     }
     public void isChief(List<Soldier> soldiers) {
